d2d9b7011113aa352632a6c283c7bcdf179e143a
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-12-29 00:32:18 +0530

    QR Scanner added

--- src/components/home/AbsoluteQRBottom.tsx
+++ src/components/home/AbsoluteQRBottom.tsx
@@ -3,23 +3,29 @@ import { TouchableOpacity, View } from "react-native";
 import { bottomTabStyles } from "../../styles/bottomTabStyle";
 import Icon from "../global/Icon";
 import { navigate } from "../../utils/NavigationUtil";
+import QRScannerModal from "../modals/QRScannerModal";
 
 const AbsoluteQRBottom: FC = () => {
 
-    const [visible, setVisible] = useState<boolean>(false);
+    const [isVisible, setIsVisible] = useState<boolean>(false);
 
     return (
-        <View style={bottomTabStyles.container}>
-            <TouchableOpacity onPress={() => navigate("ReceivedFileScreen")}>
-                <Icon name="apps-sharp" iconFamily="Ionicons" color="#333" size={24} />
-            </TouchableOpacity>
-            <TouchableOpacity style={bottomTabStyles.qrCode} onPress={() => setVisible(true)}>
-                <Icon name="qrcode-scan" iconFamily="MaterialCommunityIcons" color="#fff" size={26} />
-            </TouchableOpacity>
-            <TouchableOpacity>
-                <Icon name="beer-sharp" iconFamily="Ionicons" color="#333" size={24} />
-            </TouchableOpacity>
-        </View>
+        <>
+            <View style={bottomTabStyles.container}>
+                <TouchableOpacity onPress={() => navigate("ReceivedFileScreen")}>
+                    <Icon name="apps-sharp" iconFamily="Ionicons" color="#333" size={24} />
+                </TouchableOpacity>
+                <TouchableOpacity style={bottomTabStyles.qrCode} onPress={() => setIsVisible(true)}>
+                    <Icon name="qrcode-scan" iconFamily="MaterialCommunityIcons" color="#fff" size={26} />
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Icon name="beer-sharp" iconFamily="Ionicons" color="#333" size={24} />
+                </TouchableOpacity>
+            </View>
+            {
+                isVisible ? <QRScannerModal visible={isVisible} onClose={() => setIsVisible(false)} /> : <></>
+            }
+        </>
     );
 };
 
