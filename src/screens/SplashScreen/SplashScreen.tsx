61593af106086ae8966189dc584ca4fba36c422d
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-12-28 18:48:00 +0530

    splash screen added

---
+++ src/screens/SplashScreen/SplashScreen.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect } from "react";
+import { View, Image } from "react-native";
+import { commonStyles } from "../../styles/commonStyles";
+import { navigate } from "../../utils/NavigationUtil";
+
+const SplashScreen = () => {
+
+    const navigateToHome = () => {
+        navigate("HomeScreen");
+    };
+
+    useEffect(() => {
+        const timeoutId = setTimeout(navigateToHome, 1200);
+        return () => clearTimeout(timeoutId);
+    });
+
+    return (
+        <View style={commonStyles.container}>
+            <Image style={commonStyles.img} source={require("../../assets/images/logo_text.png")} />
+        </View>
+    );
+};
+
+export default SplashScreen;
