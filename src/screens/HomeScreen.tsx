881a5016800a579c1a8aba29be20b679e7aca07a
Ashar Ali Khan <asharalikhan@Ashars-MacBook-Air.local>
2024-12-28 22:58:36 +0530

    Options added

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,11 +2,18 @@ import React, { FC } from "react";
 import { View } from "react-native";
 import { commonStyles } from "../styles/commonStyles";
 import HomeHeader from "../components/home/HomeHeader";
+import { ScrollView } from "react-native";
+import SendReceiveButton from "../components/home/SendReceiveButton";
+import Options from "../components/home/Options";
 
 const HomeScreen: FC = () => {
     return (
         <View style={commonStyles.baseContainer}>
             <HomeHeader />
+            <ScrollView contentContainerStyle={{ paddingBottom: 100, padding: 15 }} showsVerticalScrollIndicator={false}>
+                <SendReceiveButton />
+                <Options isHome={true} />
+            </ScrollView>
         </View>
     );
 };
